30f2b64910604e7bbc43f2613be2beb1d00df628
asaad227 <a.saad227@yahoo.com>
2022-03-27 23:34:03 +0100

    update home components to delete Nav

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../index.css';
 import './index.css';
-import Nav from '../Nav';
 import pic from "./ahammedSaad (3).png"
 import TypeAnimation from 'react-type-animation';
 import '../Nav/index.css';
