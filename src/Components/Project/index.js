e30d0474cd9161f4d28110ab2760b87f32ff51f3
asaad227 <a.saad227@yahoo.com>
2022-06-06 11:15:56 +0100

    update css @media screen and added project txt heading

--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -40,18 +40,24 @@ export default function Project() {
           <a href="https://simple-app2.netlify.app/">
             <i className="fa"></i>Simple App
           </a>
+          <p>Full stack react app with CRUD functionality. 
+          Frontend react.js and backend using PostgreSQL. 
+          Features such as darkmode, i18n translator, hide & show component, progressive graph </p>
           <img src={simpleAll} alt="Simple App" loading="lazy" />
         </div>
         <div className="flex-box">
           <a href="https://pokemon-q.vercel.app/">
             <i className="fa"></i>Pokemon-Q
           </a>
+          <p>Fetch Api and JavaScript DOM manipulation to dynamically 
+          access and update the content, structure and style of the document.</p>
           <img src={PokemonQ} alt="Pokemon-Q" loading="lazy" />
         </div>
         <div className="flex-box">
           <a href="https://bootcampers-social.netlify.app/">
             <i className="fa"></i> Bootcampers Social App
           </a>
+          <p>Full Stack React App with CRUD functionality.</p>
           <img src={Bootcampers1} alt="Bootcampers Social App" loading="lazy" />
         </div>
         <div className="flex-box">
@@ -64,6 +70,7 @@ export default function Project() {
           <a href="https://my-pigeon-blogs.vercel.app/">
             <i className="fa"></i>My Pigeon blog APP
           </a>
+          <p>Simple Next.js app using dummy data to making blog post.</p>
           <img src={Pigeon1} alt="My Pigeon Blogs" loading="lazy" />
         </div>
        
@@ -72,6 +79,7 @@ export default function Project() {
           <a href="https://poetry-finder.netlify.app/">
             <i className="fa"></i>Poetry Finder APP
           </a>
+          <p>Using Next.js to fetch data and design with css.module.</p>
           <img src={poetry} alt="Poetry Finder" loading="lazy" />
         </div>
       </div>
