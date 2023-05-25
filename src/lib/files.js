e5644a6c36f9d752300f917f50b70e0a9e6bde04
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-05-25 13:57:30 +0100

    weather app project added

--- src/lib/files.js
+++ src/lib/files.js
@@ -1,60 +1,77 @@
 
 const myData =[{
     id:1,
+    name:"weather app Desktop",
+    url:"https://myweatherapptypescript.vercel.app/",
+    project:"https://github.com/asaad227/myweatherapptypescript/blob/main/Assets/Gif/WeatherApp-Desktop.gif?raw=true",
+},
+{   id:2,
+    name:"weather app Mobile",
+    url:"https://myweatherapptypescript.vercel.app/",
+    project:"https://github.com/asaad227/myweatherapptypescript/blob/main/Assets/Gif/WeatherApp-Mobile.gif?raw=true",
+
+},
+{
+    id:3,
     name:"My App with Auth0",
     url:"https://my-reminder-recipe.vercel.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/myApp-Auth0/My-App-with-auth0.gif?raw=true",
-},{
-    id:2,
+},
+{
+    id:4,
     name:"My Recipe App with calorie calculator",
     url:"https://my-recipe-calories-calculator.vercel.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/My-recipe-App/My-App-p1.gif?raw=true",
     text:"Card game randomly selected card for player and computer and score counted."
-},{
-    id:3,
+},
+{
+    id:5,
     name:"Simple App",
     url:"https://simple-app2.netlify.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Final-project/simpleAll.gif?raw=true",
     part:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Final-project/simpleAll.gif?raw=true",
     text:"Full stack react app with CRUD functionality.Frontend react.js and backend using PostgreSQL.Features such as darkmode, i18n translator, hide & show component, progressive graph"
 },
-
 {
-    id:4,
+    id:6,
     name:"BoxPickerKidGame",
     url:"https://box-picker.netlify.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/BoxPickerGame/box-picker-kids-netlify-leaderboard.gif?raw=true",
     text:"Box with hidden Espeon pokemon, so find the Espeon and get the level up. You have three level to choose from." 
 },
 {
-    id:5,
+    id:7,
     name:"E-commerce",
     url:"https://e-commerce-site-delta.vercel.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/E-commerce-site/E-commerce-site.gif?raw=true",
     part:"https://raw.githubusercontent.com/asaad227/portfolioApp/75a1f8adffe2f118df234604ee289cb083719af1/mynextportfolio/src/assets/Gif/E-commerce-site/E-commerce.gif?raw=true",
     text:"Fetch Api and JavaScript DOM manipulation to dynamically access and update the content, structure and style of the document."
-},{
-    id:6,
+},
+{
+    id:8,
     name:"My pigeon blogs",
     url:"https://my-pigeon-blogs.vercel.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Pigeon-blogs/Pigeon-blog-p1.gif?raw=true",
     text:"Simple Next.js app using dummy data to making blog post."
-},{
-    id:7,
+},
+{
+    id:9,
     name:"Random Password Maker",
     url:"https://randompassword-asaad227.vercel.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/RandomPassword/randomPassword.gif?raw=true",
     text:"Lets make your random password of your choice."
-},{
-    id:8,
+},
+{
+    id:10,
     name:"PokemonQ",
     url:"https://pokemon-q.vercel.app/",
     project:"https://github.com/asaad227/portfolioApp/blob/main/mynextportfolio/src/assets/Gif/Pokemon-q.gif?raw=true",
     text:"Fetch Api and JavaScript DOM manipulation to dynamically access and update the content, structure and style of the document."
    
-},{
+},
+{
      
-    id:9,
+    id:11,
     name:"Bootcamper App",
     url:"https://bootcampers-social.netlify.app/",
     project:"https://github.com/asaad227/portfolioApp/blob/main/mynextportfolio/src/assets/Gif/BootcampersSocial/bootcampersSocial-1.gif?raw=true",
