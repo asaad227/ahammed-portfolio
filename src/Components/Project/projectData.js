d18a858f73504464b79580ed5280073a1dd1e55f
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-06-01 00:39:42 +0100

    update project display to added github repo links and also update lib files

--- src/Components/Project/projectData.js
+++ src/Components/Project/projectData.js
@@ -16,7 +16,10 @@ export default function ProjectData() {
                 {data.map(e => {
                     return (<div key={e.id}>
                         <a href={e.url}>
-                            <h4>{e.name}</h4>
+                            <h4>App-Link</h4>
+                        </a>
+                        <a href={e.github}>
+                            <h4>& GitHub-Repo</h4>
                         </a>
                         <img className='projectImg' src={e.project} alt={e.name} />
                     </div>)
--- src/lib/files.js
+++ src/lib/files.js
@@ -1,62 +1,85 @@
 
 const myData =[{
     id:12,
-    name:"Tax Calculator-Desktop",
+    app:"Tax Calculator",
+    repo:"Tax Calculator",
+    github:"https://github.com/asaad227/taxCalculator",
     url:"https://tax-calculator-asaad227.vercel.app/",
     project:"https://github.com/asaad227/taxCalculator/blob/main/src/Assets/Gif/Tax-Calculator-Web.gif?raw=true",
     text:"Tax Calculator is a simple app that calculates your tax based on your income and tax bracket. It also shows you how much you will take home after tax."
 },{id:13,
-    name:"Tax Calculator-Mobile",
+    app:"Tax Calculator",
+    repo:"Tax Calculator",
+    github:"https://github.com/asaad227/taxCalculator",
     url:"https://tax-calculator-asaad227.vercel.app/",
     project:"https://github.com/asaad227/taxCalculator/blob/main/src/Assets/Gif/Tax-Calculator-Mob.gif?raw=true",
     text:"Tax Calculator is a simple app that calculates your tax based on your income and tax bracket. It also shows you how much you will take home after tax."
 
 },{
     id:1,
-    name:"Weather app Desktop",
+    app:"Weather app Desktop",
+    repo:"Weather app",
     url:"https://myweatherapptypescript.vercel.app/",
+    github:"https://github.com/asaad227/myweatherapptypescript",
     project:"https://github.com/asaad227/myweatherapptypescript/blob/main/Assets/Gif/WeatherApp-Desktop.gif?raw=true",
     text:"Weather app is a simple app that shows you the weather of your location and also you can search for any city weather and can check the weather for next 7 days."
 },
 {   id:2,
-    name:"Weather app Mobile",
+    app:"Weather app Mobile",
+    repo:"Weather app",
     url:"https://myweatherapptypescript.vercel.app/",
+    github:"https://github.com/asaad227/myweatherapptypescript",
     project:"https://github.com/asaad227/myweatherapptypescript/blob/main/Assets/Gif/WeatherApp-Mobile.gif?raw=true",
     text:"Weather app is a simple app that shows you the weather of your location and also you can search for any city weather and can check the weather for next 7 days."
 
 },
 {
     id:3,
-    name:"My App with Auth0",
+    app:"My App with Auth0",
+    repo:"My App",
     url:"https://my-reminder-recipe.vercel.app/",
+    github:"https://github.com/asaad227/my-app",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/myApp-Auth0/My-App-with-auth0.gif?raw=true",
     text:"This is a Todo and Recipe app built using Auth0, React JS, CSS, JavaScript, Material UI, and Google Fonts. The app allows users to create a list of todos with or without reminder options. Additionally, users can search for recipes and save their favorite recipes to a favorites section."
 },
 {
     id:4,
-    name:"Calories Calculator",
+    app:"Recipe & Calories Calculator",
+    repo:"Recipe & Calories Calculator",
     url:"https://my-recipe-calories-calculator.vercel.app/",
+    github:"https://github.com/asaad227/asian-recipe",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/My-recipe-App/My-App-p1.gif?raw=true",
     text:"This is a Recipe app built using React JS, CSS, JavaScript, Material UI, and Google Fonts. The app allows users to search for recipes and save their favorite recipes to a favorites section."
 },
 {
     id:5,
-    name:"Simple App",
+    app:"Simple App",
+    repo:"Simple App-backend",
     url:"https://simple-app2.netlify.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Final-project/simpleAll.gif?raw=true",
-    part:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Final-project/simpleAll.gif?raw=true",
+    github:"https://github.com/SchoolOfCode/final-project-repo-team-26-bit-by-bit",
     text:"Full stack react app with CRUD functionality.Frontend react.js and backend using PostgreSQL.Features such as darkmode, i18n translator, hide & show component, progressive graph"
+},
+{id:14,
+    app:"Simple App",
+    repo:"Simple App-Frontend",
+    url:"https://simple-app2.netlify.app/",
+    project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Final-project/simpleAll.gif?raw=true",
+    github:"https://github.com/SchoolOfCode/final-project-front-end-team-26-bit-by-bit",
+    text:"Full stack react app with CRUD functionality.Frontend react.js and backend using PostgreSQL.Features such as darkmode, i18n translator, hide & show component, progressive graph"
+
 },
 {
     id:6,
-    name:"BoxPickerKidGame",
+    app:"BoxPickerKidGame",
+    repo:"BoxPickerKidGame",
     url:"https://box-picker.netlify.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/BoxPickerGame/box-picker-kids-netlify-leaderboard.gif?raw=true",
     text:"Box with hidden Espeon pokemon, so find the Espeon and get the level up. You have three level to choose from." 
 },
 // {
 //     id:7,
-//     name:"E-commerce",
+//     app:"E-commerce",
 //     url:"https://e-commerce-site-delta.vercel.app/",
 //     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/E-commerce-site/E-commerce-site.gif?raw=true",
 //     part:"https://raw.githubusercontent.com/asaad227/portfolioApp/75a1f8adffe2f118df234604ee289cb083719af1/mynextportfolio/src/assets/Gif/E-commerce-site/E-commerce.gif?raw=true",
@@ -64,21 +87,22 @@ const myData =[{
 // },
 // {
 //     id:8,
-//     name:"My pigeon blogs",
+//     app:"My pigeon blogs",
 //     url:"https://my-pigeon-blogs.vercel.app/",
 //     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/Pigeon-blogs/Pigeon-blog-p1.gif?raw=true",
 //     text:"This is a Blog app built using React JS, CSS, JavaScript, Material UI, and Google Fonts. The app allows users to search for blogs and save their favorite blogs to a favorites section."
 // },
 {
     id:9,
-    name:"Random Password Maker",
+    app:"Random Password Maker",
+    repo:"Random Password Maker",
     url:"https://randompassword-asaad227.vercel.app/",
     project:"https://github.com/asaad227/ahammed-portfolio/blob/main/src/assets/Gif/RandomPassword/randomPassword.gif?raw=true",
     text:"Random Password Maker is a simple app that generates a random password based on user-selected criteria. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code."
 },
 // {
 //     id:10,
-//     name:"PokemonQ",
+//     app:"PokemonQ",
 //     url:"https://pokemon-q.vercel.app/",
 //     project:"https://github.com/asaad227/portfolioApp/blob/main/mynextportfolio/src/assets/Gif/Pokemon-q.gif?raw=true",
 //     text:"PokemonQ is a simple app that generates a random pokemon and also you can search for any pokemon and can check the pokemon details."
@@ -87,7 +111,7 @@ const myData =[{
 // {
      
 //     id:11,
-//     name:"Bootcamper App",
+//     app:"Bootcamper App",
 //     url:"https://bootcampers-social.netlify.app/",
 //     project:"https://github.com/asaad227/portfolioApp/blob/main/mynextportfolio/src/assets/Gif/BootcampersSocial/bootcampersSocial-1.gif?raw=true",
 //     text:"Bootcamper App is a simple app that allows users to create a profile, add bootcamps, and add reviews to bootcamps. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code."
@@ -101,15 +125,15 @@ const myData =[{
 
 // const social = [{
 //     id: 1,
-//     name: "twitter",
+//     app: "twitter",
 //     url:"https://twitter.com/a_saad227"
 // }, {
 //     id: 2,
-//     name: "github",
+//     app: "github",
 //     url:"https://github.com/asaad227"
 // },{
 //     id:3,
-//     name:"linkedin",
+//     app:"linkedin",
 //     url:"https://www.linkedin.com/in/ahammed-saad-5341b3231/"
 // }]
 
