c4b5343a05cfbf0c0a8cd3a9fbdf2308f0225bc6
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-06-01 01:34:01 +0100

    update resume display with screen sizes

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -2,13 +2,29 @@
 import React from 'react'
 
 import "./index.css"
-// import aSaadResume from "../../assets/Images/AhammedSaadCV1.png";
-// import aSaadResume2 from "../../assets/Images/AhammedSaadCV2.png";
+import pageA from "../../assets/CV/1.png"
+import pageB from "../../assets/CV/2.png"
+import pageC from "../../assets/CV/3.png"
+import pageD from "../../assets/CV/4.png"
 import Ahammed from "../../assets/PDF/Ahammed_saad_cv_main.pdf";
 import SocialLink from '../Social-links';
 import Nav from '../Nav';
 
 function Resume() {
+function screenSizes() {
+  const width = window.innerWidth;
+  if (width <= 600) {
+    return (<div><img className='resume' src={pageA} loading='lazy' alt="Ahammed Saad Resume" />
+    <img className='resume' src={pageB} loading='lazy' alt="Ahammed Saad Resume" />
+    <img className='resume' src={pageC} loading='lazy' alt="Ahammed Saad Resume" />
+    <img className='resume' src={pageD} loading='lazy' alt="Ahammed Saad Resume" />
+    </div> )
+  }else{
+    return <iframe src={Ahammed} loading='lazy' className='resume-pdf'></iframe>
+
+  }
+ 
+}
 
 
   return (
@@ -16,10 +32,9 @@ function Resume() {
       <header className="project-nav">
    <Nav/>
       </header>
-      <iframe src={Ahammed} className='resume-pdf'></iframe>
-
-      {/* <img className='resume' src={aSaadResume} alt="Ahammed Saad Resume" />
-      <img className='resume' src={aSaadResume2} alt="Ahammed Saad Resume" /> */}
+      <div className='resume-container'>
+      {screenSizes()}
+      </div>
       <SocialLink />
     </div>
   );
