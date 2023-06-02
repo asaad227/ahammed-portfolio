3d7563d592a77b77a88ca33951aa8aea9ef3caf0
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-06-02 22:22:38 +0100

    update css project display and fixed toggle darkmode box shadow

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -4,41 +4,44 @@ import { Link } from 'react-router-dom'
 export default function Nav() {
   const [toggle, setToggle] = useState(false);
 
-  function darkMode(bgMain, txtMain, txtSecond, bgCol, bgWhite, txtThird) {
- 
-    setToggle(!toggle)
-     if(!toggle){
-       bgMain = "#fff";
-       txtMain = "#000";
-       txtSecond = "#000";
-       bgCol = "#fff";
-       bgWhite = "#000";
-       txtThird = "#000";
-       document.documentElement.style.setProperty("--bg-lightBlue", bgMain);
-       document.documentElement.style.setProperty("--text-main", txtMain);
-       document.documentElement.style.setProperty("--text-second", txtSecond);
-       document.documentElement.style.setProperty("--bg-color", bgCol);
-       document.documentElement.style.setProperty("--bg-white", bgWhite);
-       document.documentElement.style.setProperty("--text-third", txtThird);
-     }else{
-       document.documentElement.style.setProperty("--bg-lightBlue", "#003366");
-       document.documentElement.style.setProperty("--text-main", "#fff");
-       document.documentElement.style.setProperty("--text-second", "#FFFF00");
-       document.documentElement.style.setProperty("--bg-color", "#111");
-       document.documentElement.style.setProperty("--bg-white", "#ffffff");
-       document.documentElement.style.setProperty("--text-third", "#990099");
-     }
-     /*
-       --bg-lightBlue:#003366;
-   --text-main : #fff;
-   --text-second: #FFFF00;
-   --bg-color: #111;
-   --bg-white:#ffffff;
-   --text-third:##990099;  
-      */
- 
- 
-   }
+  function darkMode(bgMain, txtMain, txtSecond, bgCol, bgWhite, txtThird, bgBox) {
+    setToggle(!toggle);
+    if (!toggle) {
+      bgMain = "#fff";
+      txtMain = "#000";
+      txtSecond = "#000";
+      bgCol = "#fff";
+      bgWhite = "#000";
+      txtThird = "#000";
+      bgBox = "rgba(0, 0, 0, 0.2)";
+      document.documentElement.style.setProperty("--bg-lightBlue", bgMain);
+      document.documentElement.style.setProperty("--text-main", txtMain);
+      document.documentElement.style.setProperty("--text-second", txtSecond);
+      document.documentElement.style.setProperty("--bg-color", bgCol);
+      document.documentElement.style.setProperty("--bg-white", bgWhite);
+      document.documentElement.style.setProperty("--text-third", txtThird);
+      document.documentElement.style.setProperty("--bg-box2", bgBox);
+    } else {
+      document.documentElement.style.setProperty("--bg-lightBlue", "#003366");
+      document.documentElement.style.setProperty("--text-main", "#fff");
+      document.documentElement.style.setProperty("--text-second", "#FFFF00");
+      document.documentElement.style.setProperty("--bg-color", "#111");
+      document.documentElement.style.setProperty("--bg-white", "#ffffff");
+      document.documentElement.style.setProperty("--text-third", "#990099");
+      document.documentElement.style.setProperty(
+        "--bg-box2", 'rgba(255, 255, 255, 0.2)'  
+      );
+    }
+
+
+    /**  --bg-lightBlue:#003366;
+  --text-main : #fff;
+  --text-second: #FFFF00;
+  --bg-color: #111;
+  --bg-white:#ffffff;
+  --text-third:##990099;
+  --bg-box2:rgba(255, 255, 255, 0.2); */
+  }
    
 return (
     <header className='menuBar'> 
