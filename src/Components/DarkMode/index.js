585073f173b88a073cddf84bfdea3892aa5555bd
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-06-02 22:54:17 +0100

    darkmode component created and update home and nav component

---
+++ src/Components/DarkMode/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function DarkMode({toggle, setToggle}) {
+    
+
+  function darkMode(bgMain, txtMain, txtSecond, bgCol, bgWhite, txtThird, bgBox) {
+    setToggle(!toggle);
+    if (!toggle) {
+      bgMain = "#fff";
+      txtMain = "#000";
+      txtSecond = "#990099";
+      bgCol = "#fff";
+      bgWhite = "#000";
+      txtThird = "#000";
+      bgBox = "rgba(0, 0, 0, 0.2)";
+      document.documentElement.style.setProperty("--bg-lightBlue", bgMain);
+      document.documentElement.style.setProperty("--text-main", txtMain);
+      document.documentElement.style.setProperty("--text-second", txtSecond);
+      document.documentElement.style.setProperty("--bg-color", bgCol);
+      document.documentElement.style.setProperty("--bg-white", bgWhite);
+      document.documentElement.style.setProperty("--text-third", txtThird);
+      document.documentElement.style.setProperty("--bg-box2", bgBox);
+    } else {
+      document.documentElement.style.setProperty("--bg-lightBlue", "#003366");
+      document.documentElement.style.setProperty("--text-main", "#fff");
+      document.documentElement.style.setProperty("--text-second", "#FFFF00");
+      document.documentElement.style.setProperty("--bg-color", "#111");
+      document.documentElement.style.setProperty("--bg-white", "#ffffff");
+      document.documentElement.style.setProperty("--text-third", "#990099");
+      document.documentElement.style.setProperty(
+        "--bg-box2", 'rgba(255, 255, 255, 0.2)'  
+      );
+    }
+
+
+    /**  --bg-lightBlue:#003366;
+  --text-main : #fff;
+  --text-second: #FFFF00;
+  --bg-color: #111;
+  --bg-white:#ffffff;
+  --text-third:##990099;
+  --bg-box2:rgba(255, 255, 255, 0.2); */
+  }
+  
+  return (
+    <div >
+    <i className={!toggle? "fas fa-toggle-on":"fas fa-toggle-off"} onClick={darkMode}></i>
+   </div>
+  )
+}
