093bc75023be6bb8c275a63d1235d0e8a4e8ff21
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-06-02 23:09:40 +0100

    update white mode default

--- src/Components/DarkMode/index.js
+++ src/Components/DarkMode/index.js
@@ -3,43 +3,43 @@ import React from 'react'
 export default function DarkMode({ toggle, setToggle }) {
 
 
-    function darkMode(bgMain, txtMain, txtSecond, bgCol, bgWhite, txtThird, bgBox) {
+    function darkMode(bgMain, txtMain, txtSecond, bgCol, bgBlack, txtThird, bgBox) {
         setToggle(!toggle);
         if (!toggle) {
-            bgMain = "#fff";
-            txtMain = "#000";
-            txtSecond = "#990099";
+            bgMain ="#003366";
+            txtMain =  "#fff";
+            txtSecond ="#FFFF00" ;
             bgCol = "#fff";
-            bgWhite = "#000";
-            txtThird = "#000";
-            bgBox = "rgba(0, 0, 0, 0.2)";
+            bgBlack = "#111";
+            txtThird =  "#990099";
+            bgBox = 'rgba(255, 255, 255, 0.2)';
             document.documentElement.style.setProperty("--bg-main", bgMain);
             document.documentElement.style.setProperty("--text-main", txtMain);
             document.documentElement.style.setProperty("--text-second", txtSecond);
             document.documentElement.style.setProperty("--bg-color", bgCol);
-            document.documentElement.style.setProperty("--bg-white", bgWhite);
+            document.documentElement.style.setProperty("--bg-black", bgBlack);
             document.documentElement.style.setProperty("--text-third", txtThird);
             document.documentElement.style.setProperty("--bg-box2", bgBox);
         } else {
-            document.documentElement.style.setProperty("--bg-main", "#003366");
-            document.documentElement.style.setProperty("--text-main", "#fff");
-            document.documentElement.style.setProperty("--text-second", "#FFFF00");
-            document.documentElement.style.setProperty("--bg-color", "#111");
-            document.documentElement.style.setProperty("--bg-white", "#ffffff");
-            document.documentElement.style.setProperty("--text-third", "#990099");
+            document.documentElement.style.setProperty("--bg-main", "#ffffff");
+            document.documentElement.style.setProperty("--text-main","#000");
+            document.documentElement.style.setProperty("--text-second","#990099" );
+            document.documentElement.style.setProperty("--bg-color","#ffffff" );
+            document.documentElement.style.setProperty("--bg-black",'#000' );
+            document.documentElement.style.setProperty("--text-third",'#FFFF00' );
             document.documentElement.style.setProperty(
-                "--bg-box2", 'rgba(255, 255, 255, 0.2)'
+                "--bg-box2",'rgba(0, 0, 0, 0.2)' 
             );
         }
 
 
-        /**  --bg-main:#003366;
-      --text-main : #fff;
-      --text-second: #FFFF00;
-      --bg-color: #111;
-      --bg-white:#ffffff;
-      --text-third:##990099;
-      --bg-box2:rgba(255, 255, 255, 0.2); */
+        /**  --bg-main:#ffffff;
+  --text-main : #000;
+  --text-second: #990099;
+  --bg-color:#ffffff;
+  --bg-black:#000;
+  --text-third:#FFFF00;
+  --bg-box2:rgba(0, 0, 0, 0.2); */
     }
 
     return (
